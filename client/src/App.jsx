62347c4055fbf2ef46a3773c6990acfffd94ce3f
Rafi Rajoyan <rafirjr@gmail.com>
2022-08-20 11:42:31 -0700

    Set up client side with npx create-react-app

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+import {BrowserRouter as Router, Routes, Route, Redirect} from "react-router-dom";
+// Switch is now Routes
+
+function App() {
+  return (
+    <Fragment>
+       
+    </Fragment>
+  );
+}
+
+export default App;
